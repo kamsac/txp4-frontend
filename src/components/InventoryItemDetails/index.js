186cb666d21d8b8025846338404f992eac927495
kamsac <kamsac@users.noreply.github.com>
2018-03-05 19:39:58 +0100

    Refactor InventoryItemDetails.

--- src/components/InventoryItemDetails/index.js
+++ src/components/InventoryItemDetails/index.js
@@ -9,31 +9,14 @@ export default class InventoryItemDetails extends React.Component {
     const item = this.props.item;
 
     if (item) {
-      const stats = item.modifiers.map((modifier) => {
-        if (modifier.value !== 1) {
-          const percent = Math.round(modifier.value * 100) + '%';
-          return (
-            <li key={modifier.name}>{`${percent} of ${modifier.name}`}</li>
-          );
-        }
-      });
-
-      let itemName;
-      if (item.vendor) {
-        itemName = (
-          <h1><span style={{color: item.vendor.color}}>{item.vendor.name}</span> {item.name} (tier {item.tier})</h1>
-        );
-      } else {
-        itemName = (
-          <h1>{item.name} (tier {item.tier})</h1>
-        );
-      }
+      const itemName = renderItemName(item);
+      const itemModifiers = renderItemModifiers(item);
 
       return (
         <div className="InventoryItemDetails">
           {itemName}
           <ul>
-            {stats}
+            {itemModifiers}
           </ul>
         </div>
       );
@@ -45,4 +28,27 @@ export default class InventoryItemDetails extends React.Component {
       )
     }
   }
+}
+
+function renderItemName(item) {
+  if (item.vendor) {
+    return (
+      <h1><span style={{color: item.vendor.color}}>{item.vendor.name}</span> {item.name} (tier {item.tier})</h1>
+    );
+  } else {
+    return (
+      <h1>{item.name} (tier {item.tier})</h1>
+    );
+  }
+}
+
+function renderItemModifiers(item) {
+  return item.modifiers.map((modifier) => {
+    if (modifier.value !== 1) {
+      const percent = Math.round(modifier.value * 100) + '%';
+      return (
+        <li key={modifier.name}>{`${percent} of ${modifier.name}`}</li>
+      );
+    }
+  });
 }
