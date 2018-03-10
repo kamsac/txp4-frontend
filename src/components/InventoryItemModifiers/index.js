8fa6ec2d1da89850193c3a3c1bc355089571e505
kamsac <kamsac@users.noreply.github.com>
2018-03-10 15:41:29 +0100

    Colorize item modifiers.

--- src/components/InventoryItemModifiers/index.js
+++ src/components/InventoryItemModifiers/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './styles.scss';
+import classNames from 'classnames';
 
 export default class InventoryItemModifiers extends React.Component {
   constructor(props) {
@@ -9,10 +10,15 @@ export default class InventoryItemModifiers extends React.Component {
   render() {
     const modifiers = this.props.item.modifiers
       .filter((modifier) => modifier.value !== 1)
+      .sort((modifierA, modifierB) => modifierB.value - modifierA.value)
       .map((modifier) => {
         const percent = Math.round(modifier.value * 100) + '%';
+        const className = classNames('InventoryItemModifiers-modifier', {
+          'InventoryItemModifiers-modifier--positive': modifier.value > 1,
+          'InventoryItemModifiers-modifier--negative': modifier.value < 1,
+        });
         return (
-          <li key={modifier.name}>{`${percent} of ${modifier.name}`}</li>
+          <li key={modifier.name} className={className}>{`${percent} of ${modifier.name}`}</li>
         );
       });
 
