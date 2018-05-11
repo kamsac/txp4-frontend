0d217c29d16d69609396582894317f0eb8f98de3
kamsac <kamsac@users.noreply.github.com>
2018-05-11 21:01:52 +0200

    Remove eslint-loader from webpack.

--- webpack.config.js
+++ webpack.config.js
@@ -18,12 +18,6 @@ module.exports = {
   module: {
     rules: [
       {
-        enforce: 'pre',
-        test: /\.jsx?$/,
-        exclude: /node_modules/,
-        loader: 'eslint-loader',
-      },
-      {
         test: /\.jsx?$/,
         exclude: /node_modules/,
         loader: 'babel-loader',
